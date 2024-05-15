42dc7eaa0784d35a4703ba746f5b6b4a332a7b57
jaesekim <jaeseki@gmail.com>
2024-05-15 21:39:09 +0900

    :tada: Start: 프로젝트 시작

---
+++ CryptoCoinMarket/CryptoCoinMarket/CryptoCoinMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinMarketApp.swift
+//  CryptoCoinMarket
+//
+//  Created by 김재석 on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCoinMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
