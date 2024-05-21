eb04b2e1da40b401382d55a1d569a68c9fd91d77
jaesekim <jaeseki@gmail.com>
2024-05-21 21:04:13 +0900

    Feat: ViewModelType 프로토콜 생성

---
+++ CryptoCoinMarket/CryptoCoinMarket/Protocol/ViewModelType.swift
@@ -0,0 +1,22 @@
+//
+//  ViewModelType.swift
+//  CryptoCoinMarket
+//
+//  Created by 김재석 on 5/21/24.
+//
+
+import Foundation
+import Combine
+
+protocol ViewModelType: ObservableObject {
+    
+    associatedtype Input
+    associatedtype Output
+
+    var cancellable: Set<AnyCancellable> { get set }
+    
+    var input: Input { get set }
+    var output: Output { get set }
+    
+    func transform()
+}
